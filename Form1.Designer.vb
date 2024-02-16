bccf33ce9764d516218e272f16fe7d7356af2bf0
yudhi <yudhi@YUDHIW11PARALLE>
2024-02-16 10:42:10 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormTaskManager
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBoxTasks = New ListBox()
+        GroupBox1 = New GroupBox()
+        ButtonDisplayTasks = New Button()
+        ButtonAddTask = New Button()
+        ButtonEditTask = New Button()
+        ButtonDeleteTask = New Button()
+        ButtonCompleteTask = New Button()
+        Label1 = New Label()
+        ButtonViewSelesai = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBoxTasks
+        ' 
+        ListBoxTasks.FormattingEnabled = True
+        ListBoxTasks.Location = New Point(255, 114)
+        ListBoxTasks.Name = "ListBoxTasks"
+        ListBoxTasks.Size = New Size(522, 516)
+        ListBoxTasks.TabIndex = 0
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Location = New Point(120, 232)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(8, 8)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' ButtonDisplayTasks
+        ' 
+        ButtonDisplayTasks.Location = New Point(255, 23)
+        ButtonDisplayTasks.Name = "ButtonDisplayTasks"
+        ButtonDisplayTasks.Size = New Size(249, 61)
+        ButtonDisplayTasks.TabIndex = 2
+        ButtonDisplayTasks.Text = "Daftar Tugas Berjalan"
+        ButtonDisplayTasks.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonAddTask
+        ' 
+        ButtonAddTask.Location = New Point(28, 114)
+        ButtonAddTask.Name = "ButtonAddTask"
+        ButtonAddTask.Size = New Size(206, 46)
+        ButtonAddTask.TabIndex = 3
+        ButtonAddTask.Text = "Tambah Tugas"
+        ButtonAddTask.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonEditTask
+        ' 
+        ButtonEditTask.Location = New Point(28, 166)
+        ButtonEditTask.Name = "ButtonEditTask"
+        ButtonEditTask.Size = New Size(206, 46)
+        ButtonEditTask.TabIndex = 4
+        ButtonEditTask.Text = "Edit Tugas"
+        ButtonEditTask.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDeleteTask
+        ' 
+        ButtonDeleteTask.Location = New Point(28, 218)
+        ButtonDeleteTask.Name = "ButtonDeleteTask"
+        ButtonDeleteTask.Size = New Size(206, 46)
+        ButtonDeleteTask.TabIndex = 5
+        ButtonDeleteTask.Text = "Hapus Tugas"
+        ButtonDeleteTask.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonCompleteTask
+        ' 
+        ButtonCompleteTask.Location = New Point(28, 270)
+        ButtonCompleteTask.Name = "ButtonCompleteTask"
+        ButtonCompleteTask.Size = New Size(206, 46)
+        ButtonCompleteTask.TabIndex = 6
+        ButtonCompleteTask.Text = "Tandai Selesai"
+        ButtonCompleteTask.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(150, 390)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 32)
+        Label1.TabIndex = 7
+        ' 
+        ' ButtonViewSelesai
+        ' 
+        ButtonViewSelesai.Location = New Point(528, 23)
+        ButtonViewSelesai.Name = "ButtonViewSelesai"
+        ButtonViewSelesai.Size = New Size(249, 61)
+        ButtonViewSelesai.TabIndex = 9
+        ButtonViewSelesai.Text = "Daftar Tugas Selesai"
+        ButtonViewSelesai.UseVisualStyleBackColor = True
+        ' 
+        ' FormTaskManager
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(808, 660)
+        Controls.Add(ButtonViewSelesai)
+        Controls.Add(Label1)
+        Controls.Add(ButtonCompleteTask)
+        Controls.Add(ButtonDeleteTask)
+        Controls.Add(ButtonEditTask)
+        Controls.Add(ButtonAddTask)
+        Controls.Add(ButtonDisplayTasks)
+        Controls.Add(GroupBox1)
+        Controls.Add(ListBoxTasks)
+        MaximizeBox = False
+        MaximumSize = New Size(834, 731)
+        Name = "FormTaskManager"
+        Text = "Tugas"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBoxTasks As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ButtonDisplayTasks As Button
+    Friend WithEvents ButtonAddTask As Button
+    Friend WithEvents ButtonEditTask As Button
+    Friend WithEvents ButtonDeleteTask As Button
+    Friend WithEvents ButtonCompleteTask As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonViewSelesai As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,138 @@
+﻿Imports System.IO
+Public Class FormTaskManager
+    ' Deklarasi variabel global
+    Dim tasks As New List(Of String)
+    Dim completedTasks As New List(Of String)
+
+    ' Fungsi untuk membaca daftar tugas dari file teks
+    Private Sub ReadTasksFromFile()
+        If File.Exists("tasks.txt") Then
+            tasks = File.ReadAllLines("tasks.txt").ToList()
+        End If
+    End Sub
+
+    ' Fungsi untuk menyimpan daftar tugas ke file teks
+    Private Sub SaveTasksToFile()
+        File.WriteAllLines("tasks.txt", tasks)
+    End Sub
+
+    ' Prosedur untuk menampilkan daftar tugas
+    Private Sub DisplayTasks(taskList As List(Of String), taskType As String)
+        ListBoxTasks.Items.Clear()
+        ListBoxTasks.Items.Add($"Daftar Tugas {taskType}:")
+        For Each taskItem As String In taskList
+            ListBoxTasks.Items.Add(taskItem)
+        Next
+    End Sub
+
+    ' Prosedur untuk menambah tugas
+    Private Sub AddTask()
+        Dim newTask As String = InputBox("Masukkan tugas baru:", "Tambah Tugas")
+        If Not String.IsNullOrEmpty(newTask) Then
+            tasks.Add(newTask)
+            MessageBox.Show("Tugas berhasil ditambahkan.", "Informasi")
+        End If
+    End Sub
+
+    ' Prosedur untuk mengedit tugas
+    Private Sub EditTask()
+        If ListBoxTasks.SelectedIndex <> -1 Then
+            Dim selectedIndex As Integer = ListBoxTasks.SelectedIndex
+            Dim editedTask As String = InputBox($"Edit tugas: {tasks(selectedIndex - 1)}", "Edit Tugas", tasks(selectedIndex - 1))
+            If Not String.IsNullOrEmpty(editedTask) Then
+                tasks(selectedIndex - 1) = editedTask
+                MessageBox.Show("Tugas berhasil diubah.", "Informasi")
+            End If
+        Else
+            MessageBox.Show("Pilih tugas yang akan diubah.", "Peringatan")
+        End If
+    End Sub
+
+    ' Prosedur untuk menghapus tugas
+    Private Sub DeleteTask()
+        If ListBoxTasks.SelectedIndex <> -1 Then
+            Dim selectedIndex As Integer = ListBoxTasks.SelectedIndex
+            tasks.RemoveAt(selectedIndex - 1)
+            MessageBox.Show("Tugas berhasil dihapus.", "Informasi")
+        Else
+            MessageBox.Show("Pilih tugas yang akan dihapus.", "Peringatan")
+        End If
+    End Sub
+
+    ' Prosedur untuk menandai tugas sebagai selesai
+    Private Sub CompleteTask()
+        If ListBoxTasks.SelectedIndex <> -1 Then
+            Dim selectedIndex As Integer = ListBoxTasks.SelectedIndex
+            completedTasks.Add(tasks(selectedIndex - 1))
+            tasks.RemoveAt(selectedIndex - 1)
+            MessageBox.Show("Tugas berhasil ditandai sebagai selesai.", "Informasi")
+        Else
+            MessageBox.Show("Pilih tugas yang akan ditandai sebagai selesai.", "Peringatan")
+        End If
+    End Sub
+
+    ' Prosedur untuk disable button saat view tugas selesai
+    Private Sub DisableButton()
+        ButtonAddTask.Enabled = False
+        ButtonCompleteTask.Enabled = False
+        ButtonEditTask.Enabled = False
+        ButtonDeleteTask.Enabled = False
+    End Sub
+
+    ' Prosedur untuk enable button saat view tugas selesai
+    Private Sub EnableButton()
+        ButtonAddTask.Enabled = True
+        ButtonCompleteTask.Enabled = True
+        ButtonEditTask.Enabled = True
+        ButtonDeleteTask.Enabled = True
+    End Sub
+
+
+    ' Handler untuk menampilkan daftar tugas saat form dimuat
+    Private Sub FormTaskManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReadTasksFromFile()
+        DisplayTasks(tasks, "Belum Selesai")
+    End Sub
+
+    ' Handler untuk tombol Tampilkan Tugas
+    Private Sub ButtonDisplayTasks_Click(sender As Object, e As EventArgs) Handles ButtonDisplayTasks.Click
+        DisplayTasks(tasks, "Belum Selesai")
+        EnableButton()
+    End Sub
+
+    ' Handler untuk melihat tugas yang sudah selesai
+    Private Sub ButtonViewSelesai_Click(sender As Object, e As EventArgs) Handles ButtonViewSelesai.Click
+        DisplayTasks(completedTasks, "Selesai")
+        DisableButton()
+    End Sub
+
+    ' Handler untuk tombol Tambah Tugas
+    Private Sub ButtonAddTask_Click(sender As Object, e As EventArgs) Handles ButtonAddTask.Click
+        AddTask()
+        DisplayTasks(tasks, "Belum Selesai")
+    End Sub
+
+    ' Handler untuk tombol Edit Tugas
+    Private Sub ButtonEditTask_Click(sender As Object, e As EventArgs) Handles ButtonEditTask.Click
+        EditTask()
+        DisplayTasks(tasks, "Belum Selesai")
+    End Sub
+
+    ' Handler untuk tombol Hapus Tugas
+    Private Sub ButtonDeleteTask_Click(sender As Object, e As EventArgs) Handles ButtonDeleteTask.Click
+        DeleteTask()
+        DisplayTasks(tasks, "Belum Selesai")
+    End Sub
+
+    ' Handler untuk tombol Tandai Selesai
+    Private Sub ButtonCompleteTask_Click(sender As Object, e As EventArgs) Handles ButtonCompleteTask.Click
+        CompleteTask()
+        DisplayTasks(tasks, "Belum Selesai")
+    End Sub
+
+    ' Handler untuk menyimpan tugas saat form ditutup
+    Private Sub FormTaskManager_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        SaveTasksToFile()
+    End Sub
+
+End Class
